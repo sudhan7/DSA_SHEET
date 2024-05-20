46a7e8f75b6316703dbc0667a2ed3694d07a3fd7
Sudhan <70229640+sudhan7@users.noreply.github.com>
2024-05-20 11:55:16 +0530

    Time: 39 ms (41.12%), Space: 16.6 MB (57.60%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,19 +1,16 @@
 class Solution:
-    def ncr(self,n, r):
-        res = 1
-        for i in range(r):
-            res = res * (n-i)
-            res = res // (i+1)
-        return int(res)
-    
-    def generate(self, n: int) -> List[List[int]]:
-        ans = []
+    def generaterow(self, row):
+        ans = 1
+        ansRow = [1]
         
-        for row in range(1, n+1):
-            temp = []
-            for col in range(1, row+1):
-                temp.append(self.ncr(row-1, col-1))
-            ans.append(temp)
+        for col in range(1,row):
+            ans = ans * (row - col)
+            ans = ans // col
+            ansRow.append(ans)
+        return ansRow
+    
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans = []
+        for row in range(1,numRows+1):
+            ans.append(self.generaterow(row))
         return ans
-    
-    
