6d4dbccc75ba77234cbbab0a6b4d4f4f1d2796a4
Sudhan <70229640+sudhan7@users.noreply.github.com>
2024-05-21 11:06:28 +0530

    Time: 45 ms (44.11%), Space: 16.8 MB (13.11%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,22 +1,21 @@
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        n = len(nums)
-        ind = -1 
-        for i in range(n-2, -1, -1):
-            if nums[i] < nums[i+1]:
+    def nextPermutation(self, A: List[int]) -> None:
+        n = len(A)
+        ind = -1
+        
+        for i in range(n-2,-1,-1):
+            if A[i] < A[i+1]:
                 ind = i
                 break
-        
+                
         if ind == -1:
-            nums.reverse()
-            return nums
+            A.reverse()
+            return A
         
         for i in range(n-1, ind, -1):
-            if nums[i] > nums[ind]:
-                nums[i], nums[ind] = nums[ind],  nums[i]
+            if A[i] > A[ind]:
+                A[i], A[ind] = A[ind], A[i]
                 break
-        
-        nums[ind+1:] = reversed(nums[ind+1:])
-        
-        return nums
-        
+                
+        A[ind+1:] = reversed(A[ind+1:])
+        return A
